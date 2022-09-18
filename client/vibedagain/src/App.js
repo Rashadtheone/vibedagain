67c1b843918445eb02f1521f13f0ae0d00cfd9ae
Rashadtheone <rashad.hammonds0114@gmail.com>
2022-09-18 10:35:16 -0400

    first commit, updating style.

--- client/vibedagain/src/App.js
+++ client/vibedagain/src/App.js
@@ -1,17 +1,112 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [artistName, setArtistName] = useState("")
+  const [favSong, setFavSong] = useState("")
+  const [userNote, setUserNote] = useState("")
+  const [favImage, setFavImage] = useState("")
+
+  const [artistList, setArtistList] = useState([])
+
+  const [newartistName, setNewArtistName] = useState('')
+  const [newfavSong, setNewFavSong] = useState('')
+  const [newuserNote, setNewUserNote] = useState('')
+  const [newfavImage, setNewFavImage] = useState('')
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/app/read').then((res => {
+      setArtistList(res.data)
+    }))
+  })
+
+  const addToList = () => {
+    axios.post('http://localhost:3001/app/addartist', {artistName: artistName, 
+    favSong: favSong, 
+    userNote: userNote, 
+    favImage: favImage})
+  }
+
+  const updateArtist = (id) => (
+    axios.put('http://localhost:3001/app/update', {
+      id: id,
+      newartistName: newartistName, 
+      newfavSong: newfavSong, 
+      newuserNote: newuserNote, 
+      newfavImage: newfavImage
+
+    })
+  )
+
+  const deleteArtist = (id) => {
+    axios.delete(`http://localhost:3001/app/delete/${id}`)
+  }
+
+
   return (
     <div className="App">
+      <h1>Who's your Favorite Artist?</h1>
+      <div className="maincontainer">
+      
       <div className='userinputs'>
+        <h1>Enter Your Here</h1>
+        <label>Artist Name</label>
+        <input type="text" onChange={(event) => {
+      setArtistName(event.target.value)
+     }}/>
+     <label>Favorite Song</label>
+      <input type="text" onChange={(event) => {
+      setFavSong(event.target.value)
+     }}/>
+     <label>A Note on them</label>
+      <input type="text" onChange={(event) => {
+      setUserNote(event.target.value)
+     }}/>
+     <label>Link to image</label>
+      <input type="text" onChange={(event) => {
+      setFavImage(event.target.value)
+     }}/>
 
-
+     
+<button onClick={addToList}>Add To List</button>
       </div>
       <div className='displaydata'>
+      {artistList.map((val, key) => {
+        return <div key = {key}>
+          <h2>{val.artistName}</h2>
+          <h2>{val.favSong}</h2>
+          <h2>{val.userNote}</h2>
+          <img src={val.favImage}/>
+          
+          <label>Artist Name</label>
+        <input type="text" onChange={(event) => {
+      setNewArtistName(event.target.value)
+     }}/>
+     <label>Favorite Song</label>
+      <input type="text" onChange={(event) => {
+      setNewFavSong(event.target.value)
+     }}/>
+     <label>A Note on them</label>
+      <input type="text" onChange={(event) => {
+      setNewUserNote(event.target.value)
+     }}/>
+     <label>Link to image</label>
+      <input type="text" onChange={(event) => {
+      setNewFavImage(event.target.value)
+     }}/>
+          
+          <button onClick={() => updateArtist(val._id)}>Update Artist</button>
 
-        
+          <button onClick={() => deleteArtist(val._id)}>Delete</button>
+          </div>
+
+      })}
+
       </div>
+    </div>
     </div>
   );
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,9 +7,9 @@ const ArtistModel = require('../models/userdata')
 router.post("/addartist", async (req, res) => {
     const artist = new ArtistModel({
      artistName : req.body.artistName,
-     favSong : req.body.artistName,
-     userNote : req.body.artistName,
-     favImage : req.body.artistName,
+     favSong : req.body.favSong,
+     userNote : req.body.userNote,
+     favImage : req.body.favImage,
 })
     
 
@@ -22,5 +22,47 @@ router.post("/addartist", async (req, res) => {
 
 
 })
+
+router.get("/read", async (req, res) => {
+    ArtistModel.find({}, (err, result) => {
+        if (err){
+            res.send(err)
+        }
+        res.send(result)
+    })
+})
+
+router.put("/update", async (req, res) => {
+    const newArtistName = req.body.newartistName
+    const newFavSong = req.body.newfavSong
+    const newUserNote= req.body.newuserNote
+    const newFavImage = req.body.newfavImage
+
+    const id = req.body.id
+
+    try {
+        await ArtistModel.findById(id, (err, updateArtist) => {
+            updateArtist.artistName = newArtistName
+            updateArtist.favSong = newFavSong
+            updateArtist.userNote = newUserNote
+            updateArtist.favImage = newFavImage 
+
+            updateArtist.save()
+            res.send("update")
+            
+        })
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+router.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+
+    await ArtistModel.findByIdAndRemove(id).exec()
+
+    res.send(id)
+})
+
 
 module.exports = router
